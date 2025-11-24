6f6c83cb9249b22e695cdf8f4d2889bca83438a8
ThiagoTEP <thiagodepaulapt@gmail.com>
2025-11-24 18:03:07 +0000

    aprendizagem para prova

---
+++ RESOLVIDOS/level 1/broken_gnl/broken_gnl.h
@@ -0,0 +1,10 @@
+#ifndef BROKEN_GNL_H
+# define BROKEN_GNL_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
---
+++ RESOLVIDOS/level 2/rip/rip_a.h
@@ -0,0 +1,8 @@
+#ifndef RIP_A_H
+# define RIP_A_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
